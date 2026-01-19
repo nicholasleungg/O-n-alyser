48273d2e575bc2977c476857d1a1e461cef16900
Nicholas Leung <nxckleung@gmail.com>
2026-01-19 18:19:09 +1100

    Completed regex expressions for java, python and c

--- web/src/engine/profiles.ts
+++ web/src/engine/profiles.ts
@@ -12,21 +12,47 @@ export type LanguageProfile = {
     
 }
 
-function stripCComments(code: string): string {
+function removeCComments(code: string): string {
     return code
         .replace(/\/\*[\s\S]*?\*\//g, "") // remove /* ... */
         .replace(/\/\/.*$/gm, "")         // remove // ...
 }
 
-function stripJavaComments(code: string): string {
+function removeJavaComments(code: string): string {
     return code
         .replace(/\/\*[\s\S]*?\*\//g, "") // remove /* ... */
         .replace(/\/\/.*$/gm, "")         // remove // ...
 }
 
-function stripPythonComments(code: string): string {
+function removePythonComments(code: string): string {
     return code
         .replace(/"""[\s\S]*?"""/g, "") // remove triple double quote blocks """..."""
         .replace(/'''[\s\S]*?'''/g, "") // remove triple single quote blocks '''...'''
         .replace(/#.*$/gm, "");         // remove # line comments
+}
+
+export const PROFILES: Record<Exclude<Lang, "auto">, LanguageProfile> = {
+    python: {
+        name: "python",
+        usesBraces: false,
+        removeComments: removePythonComments,
+        loopRegexes: [/\bfor\b/g, /\bwhile\b/g],
+        sortRegexes: [/\bsorted\s*\(/i, /\.sort\s*\(/i],
+    },
+
+    java: {
+        name: "java",
+        usesBraces: true,
+        removeComments: removeJavaComments,
+        loopRegexes: [/\bfor\b/g, /\bwhile\b/g, /\bdo\b/g],
+        sortRegexes: [/\bArrays\.sort\b/i, /\bCollections\.sort\b/i, /\.sort\s*\(/i],
+    },
+
+    c: {
+        name: "c",
+        usesBraces: true,
+        removeComments: removeCComments,
+        loopRegexes: [/\bfor\b/g, /\bwhile\b/g, /\bdo\b/g],
+        sortRegexes: [/\bqsort\s*\(/i],
+    }
 }
